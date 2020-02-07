404b06bc4db63894ee63fa5512c0ad7ee3bb80be
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-02-07 12:17:41 -0600

    annotate and rubocop

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint           not null, primary key
+#  user_id    :integer
+#  post_id    :integer
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Comment < ApplicationRecord
   validates :content, presence: true, length: { maximum: 200,
                                                 too_long: '200 characters in comment is the maximum allowed.' }
