9a45e354b514891ce528e53d8f0007f7143ea582
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-02-08 14:23:06 -0600

    ERD upgrade

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,4 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
-//= require popper
-//= require bootstrap
+
