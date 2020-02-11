c255f6b3b3561bfb843c08ec9f5e3aacfaf48a1c
mesielepush <48403308+mesielepush@users.noreply.github.com>
2020-02-11 12:12:10 -0600

    rubocop changes

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,7 +19,8 @@ require 'faker'
 
 RSpec.describe User, type: :model do
   context 'user creations validation tests' do
-    user = User.new(name: Faker::Name.name, email: Faker::Internet.email, password: 'pppppp', password_confirmation: 'pppppp')
+    user = User.new(name: Faker::Name.name, email: Faker::Internet.email,
+                    password: 'pppppp', password_confirmation: 'pppppp')
     it 'has a valid factory' do
       expect(user).to be_valid
     end
